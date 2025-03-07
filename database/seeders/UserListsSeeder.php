a6706bdccb91d43630b9b60370954943ac88adb7
Astrit02 <shalaastrit721@gmail.com>
2025-03-08 07:11:39 +1300

    fixed bug

---
+++ database/seeders/UserListsSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\UserLists;
+use Illuminate\Database\Seeder;
+
+class UserListsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $startYear = 2020;
+        $endYear = 2025;
+
+        for ($year = $startYear; $year <= $endYear; $year++) {
+            UserLists::create([
+                'list_name' => "Gjenerata ". $year
+            ]);
+        }
+    }
+}
