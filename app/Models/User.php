dd92eb3a4c39983a79f86fea93b10a7363832181
Astrit02 <shalaastrit721@gmail.com>
2025-03-11 07:16:18 +1300

    initial commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Models\UserInfo;
 
 class User extends Authenticatable
 {
@@ -53,5 +54,9 @@ class User extends Authenticatable
             'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
+    }
+
+    public function UserInfo(){
+        return $this->hasOne(UserInfo::class, 'user_id', 'id');
     }
 }
